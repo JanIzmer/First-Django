06e5675a937ae50fb6968b6e6ea569057d67e630
JanIzmer <152801742+JanIzmer@users.noreply.github.com>
2024-11-10 19:31:55 +0100

    profile
    profile

--- First Jango/firstjango/main/views.py
+++ First Jango/firstjango/main/views.py
@@ -4,3 +4,5 @@ def index (request):
     return render(request, 'main/index.html', {'title': 'Page about python'})
 def about (request):
     return render(request, 'main/about.html')
+def profile_view(request):
+    return render(request, 'main/profile.html')
