22ae78bb194b7fac11f7b33ce3a79ccec5138efd
JanIzmer <152801742+JanIzmer@users.noreply.github.com>
2024-11-11 23:58:47 +0100

    add registration/css rebuild

--- First Jango/firstjango/news/forms.py
+++ First Jango/firstjango/news/forms.py
@@ -26,7 +26,28 @@ class ArticlesForm(ModelForm):
         }
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+    
+    error_messages = {
+        'password_mismatch': 'The passwords you entered do not match. Please try again.',
+        'password_too_common': 'This password is too common. Please choose a stronger one.',
+        'password_entropic': 'Your password is too simple. Please choose a more complex password.',
+        'username_taken': 'This username is already taken. Please choose another one.',
+        'username_required': 'Username is required. Please enter a username.',
+    }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Customize individual field error messages if needed
+        self.fields['username'].error_messages = {
+            'required': 'Please enter a username.',
+            'unique': 'This username is already taken.',
+        }
+        self.fields['password1'].error_messages = {
+            'required': 'Please enter a password.',
+            'too_common': 'This password is too common. Please choose a stronger one.',
+            'password_mismatch': 'The passwords you entered do not match. Please try again.',
+        }
